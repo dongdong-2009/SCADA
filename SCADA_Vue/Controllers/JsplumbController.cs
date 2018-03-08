840515e696d4dd0a564eb08212238cb5bf09ef0d
vktun <gxzh0817@126.com>
2018-03-08 13:49:03 +0800

    use jsplumb to connect elements

---
+++ SCADA_Vue/Controllers/JsplumbController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SCADA_Vue.Controllers
+{
+    public class JsplumbController : Controller
+    {
+        // GET: Jsplumb
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
